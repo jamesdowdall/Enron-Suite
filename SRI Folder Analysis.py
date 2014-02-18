57b2b9bc3f66610971e209ad9ea5d077aa367fd3
James Dowdall <James@138-38-140-85.eduroam.bath.ac.uk>
2014-02-18 11:11:05 +0000

    After data structure change, now writing results to file

--- SRI Folder Analysis.py
+++ SRI Folder Analysis.py
@@ -3,22 +3,23 @@
 import os #import os module as it makes it easy to taverse dir structure
 import collections
 
+dictionaryOfFolders = {} #Unsorted dictionary
+listOfEmployees = [] 
+dictionaryOfEmailsInFolder = {}
+numberOfEmailsAdded = 0
+
 #Dictionary of format Folder Name - [Frequency of folder, Number of Emails in that folder]
 def getFrequencyOfFolder(folderName):
-  print dictionaryOfFolders[folderName][0]
   return dictionaryOfFolders[folderName][0]
 
 def getNumberOfEmailsInFolder(folderName):
-  print dictionaryOfFolders[folderName][1]
   return dictionaryOfFolders[folderName][1]
 
 def setFrequencyOfFolder(folderName, frequency):
-  print "Setting frequency of folder: " + folderName + " to: " + frequency
   dictionaryOfFolders[folderName][0] = frequency
   return
 
 def setNumberOfEmailsInFolder(folderName, numberOfEmails):
-  print "Setting NumberOfEmailsInFolder of folder: " + folderName + " to: " + frequency
   dictionaryOfFolders[folderName][1] = numberOfEmails
   return
 
@@ -26,33 +27,49 @@ def setNumberOfEmailsInFolder(folderName, numberOfEmails):
 
 #TODO: Change data structure of dictionaryOfFolders from folderName > frequency, to Folder Name - [Frequency of folder, Number of Emails in that folder]
 
-dictionaryOfFolders = {} #Unsorted dictionary
-listOfEmployees = [] 
-dictionaryOfEmailsInFolder = {}
-
 i = 0
-for root, dirs, files in os.walk("/Users/James/Documents/Enron Email Corpus/Base Enron Email Dataset"):#,topdown=False):
-  if i == 1:
-    listOfEmployees = dirs #Dirty way of capturing employees. 2nd Dirs is at the correct level in the file tree.
+emails = 0
+for root, dirs, files in os.walk("/Users/James/Documents/Enron Email Corpus/Base Enron Email Dataset/maildir"):#,topdown=False):
+  emails = emails + len(files)
+  if i == 0:
+    listOfEmployees = dirs #Dirty way of capturing employees. 
 
-  #if i%100 == 0:
-  #  print i
+  if i%100 == 0:
+    print i
   i = i+1
 
-  print root
-  print files
-  print len(files)
-
-  for directory in dirs:
-    if directory in dictionaryOfFolders:
-      dictionaryOfFolders[directory] = dictionaryOfFolders[directory]+1
+  if len(files) > 0:
+    folder = str(root.rpartition("/")[-1]) #folder name by parsing root
+    if folder in dictionaryOfFolders:
+      setFrequencyOfFolder(folder,getFrequencyOfFolder(folder)+1)
+      setNumberOfEmailsInFolder(folder,getNumberOfEmailsInFolder(folder)+len(files))
+      numberOfEmailsAdded = numberOfEmailsAdded+len(files)
     else:
-      dictionaryOfFolders[directory] = 1
-
+      dictionaryOfFolders[folder] = [1,len(files)]
+      numberOfEmailsAdded = numberOfEmailsAdded+len(files)
+  
 for employee in listOfEmployees:
   if employee in dictionaryOfFolders:
     del dictionaryOfFolders[employee] #Could just del, but that would throw an error, will avoid.
 
+print "Number of Emails:"
+print emails
+
+print "Number of Emails Added:"
+print numberOfEmailsAdded
+
 sortedFolders = collections.OrderedDict(sorted(dictionaryOfFolders.items(), key=lambda t: t[1]))
 print "Sorted User Defined Folders:"
 print sortedFolders
+
+results = open('SRI Analysis Results.txt', 'w+')
+titles = "Folder\t Frequency of Occurence\t Emails in folder\n"
+results.write(titles)
+for element in sortedFolders:
+  value = sortedFolders.popitem(False)
+  folder = str(value[0])
+  frequency = str(value[1][0])
+  emails = str(value[1][1])
+  formattedResults = folder + "\t" + frequency + "\t" + emails + "\n"
+  results.write (formattedResults)
+results.close()
